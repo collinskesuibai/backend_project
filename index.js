a3659edcb32808256da350f97b1a7c1fb5d1d500
Collins Kesuibai <collinskiprop13@gmail.com>
2019-11-10 20:40:13 +0300

    Deployment v-1.0.1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
-app.use('port', port);
+//app.use('port', port);
 
 app.use(bodyParser.json());
 app.use(
