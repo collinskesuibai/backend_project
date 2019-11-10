87f30f611f5fda5deaf093b2e81afb7955ba6579
Collins Kesuibai <collinskiprop13@gmail.com>
2019-11-10 20:23:32 +0300

    Deployment v-1.0.1

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const dbArticles = require('./controllers/articles');
 const dbUser = require('./controllers/users');
 const dbGif = require('./controllers/gifs');
 const auth = require('./middleware/auth');
+const cors = require('cors');
 
 const app = express();
-const port = process.env.port || 3000;
+app.use(cors());
+const port = process.env.PORT || 3000;
+app.use('port', port);
 
 app.use(bodyParser.json());
 app.use(
@@ -18,24 +21,27 @@ app.use(
     extended: true,
   }),
 );
-app.get('/gifs', dbGif.getGifs);
-app.get('/feed', dbGif.getFeed);
-app.get('/articles', dbArticles.getArticles);
-app.get('/users', dbUser.getUsers);
-app.get('/users/:id', dbUser.getUserById);
-app.get('/gifs/:id', dbGif.getGifById);
-app.get('/articles/:id', dbArticles.getArticlesById);
-app.post('/users', dbUser.createUser);
+app.get('/', function(request, response) {
+  response.send('hello world');
+});
+app.get('/gifs', auth, dbGif.getGifs);
+app.get('/feed', auth, dbGif.getFeed);
+app.get('/articles', auth, dbArticles.getArticles);
+app.get('/users', auth, dbUser.getUsers);
+app.get('/users/:id', auth, dbUser.getUserById);
+app.get('/gifs/:id', auth, dbGif.getGifById);
+app.get('/articles/:id', auth, dbArticles.getArticlesById);
+app.post('/users', auth, dbUser.createUser);
 app.post('/auth/signin', dbUser.logIn);
-app.post('/gifs/:id', dbComment.createGifComment);
-app.post('/articles/:id', dbComment.createArticleComment);
-app.post('/upload', dbGif.createGifPost);
-app.post('/articles', dbArticles.createArticle);
-app.put('/users/:id', dbUser.updateUser);
-app.patch('/articles/:id', dbArticles.updateArticles);
-app.delete('/users/:id', dbUser.deleteUser);
-app.delete('/articles/:id', dbArticles.deleteArticle);
-app.delete('/gifs/:id', dbGif.deleteGif);
+app.post('/gifs/:id', auth, dbComment.createGifComment);
+app.post('/articles/:id', auth, dbComment.createArticleComment);
+app.post('/upload', auth, dbGif.createGifPost);
+app.post('/articles', auth, dbArticles.createArticle);
+app.put('/users/:id', auth, dbUser.updateUser);
+app.patch('/articles/:id', auth, dbArticles.updateArticles);
+app.delete('/users/:id', auth, dbUser.deleteUser);
+app.delete('/articles/:id', auth, dbArticles.deleteArticle);
+app.delete('/gifs/:id', auth, dbGif.deleteGif);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
