4a88abef06efa28932e0f37b3f1fd59f375f8c1f
Collins Kesuibai <collinskiprop13@gmail.com>
2019-11-21 12:21:07 +0300

    bg-fix article-endpoint
    bug -fix article-api endpoint test

--- tests/articles.test.js
+++ tests/articles.test.js
@@ -9,7 +9,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 // const server = require('../index');
 
-// const should = chai.should();
+chai.should();
 chai.use(chaiHttp);
 
 describe('/GET articles', () => {
